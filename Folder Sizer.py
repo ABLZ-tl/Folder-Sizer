ee58b090bc0dce740e329dfadcacb7652e2a56e6
Alan Lozano <al@mdtch.mx>
2023-07-03 21:54:00 -0600

    Releasing the code

--- Folder Sizer.py
+++ Folder Sizer.py
@@ -1,31 +1,102 @@
-# Folder Size Calculator
+import os
+import time
+import multiprocessing
+import webbrowser
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk
 
-This is a Python script that calculates the size of all folders in a given directory and generates an HTML report with the results. The folders are sorted by size in descending order, so you can easily identify the largest ones.
 
-The script uses multiple processes to speed up the calculation process, and a progress bar is displayed to keep track of the progress. The results are saved in an HTML file that can be opened in any web browser. Additionally, an error log is generated in case of any exceptions.
+def get_folder_size(folder_path):
+    total_size = 0
+    for path, dirs, files in os.walk(folder_path):
+        for f in files:
+            fp = os.path.join(path, f)
+            try:
+                total_size += os.stat(fp).st_size
+            except OSError:
+                pass
+    return total_size
 
-## Usage
 
-1. Clone or download this repository.
-2. Install the required dependencies by running `pip install -r requirements.txt`.
-3. Run the script by executing `python folder_size_calculator.py`.
-4. Select the directory that you want to calculate the folder sizes for.
-5. Wait for the script to finish calculating the sizes.
-6. The results will be displayed in an HTML file called `tempfile.html` and will open in a new tab of your default web browser.
+def process_folder(folder, results):
+    size = get_folder_size(folder)
+    results.append((folder, size))
 
-Note: Make sure that the `tempfile.html` and `error-log.txt` files are not open while running the script, as they will be overwritten.
 
-## Dependencies
+def calculate_sizes(progress_label, progress_bar):
+    try:
+        input_dir = filedialog.askdirectory()
+        if not input_dir:
+            return
 
-The script requires Python 3.x and the following packages:
+        dirs = [folder for folder in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, folder))]
 
-- tkinter
-- ttk
-- multiprocessing
-- webbrowser
+        manager = multiprocessing.Manager()
+        result_list = manager.list()
 
-These dependencies can be installed by running `pip install -r requirements.txt`.
+        num_processes = multiprocessing.cpu_count()
 
-## License
+        processes = []
 
-This script is licensed under the MIT License. See the `LICENSE` file for more details.
+        for folder in dirs:
+            if __name__ == '__main__':
+                p = multiprocessing.Process(target=process_folder, args=(os.path.join(input_dir, folder), result_list))
+                p.start()
+                processes.append(p)
+
+        num_folders = len(dirs)
+
+        while len(processes) > 0:
+            time.sleep(0.1)
+            for p in processes:
+                if not p.is_alive():
+                    processes.remove(p)
+                    percent_complete = len(result_list) / num_folders * 100
+                    progress_label.config(text=f"Procesando... {len(result_list)} de {num_folders} carpetas procesadas")
+                    progress_bar["value"] = percent_complete
+                    progress_bar.update()
+
+        results = sorted(result_list, key=lambda x: x[1], reverse=True)
+
+        with open("Reporte {folder}.html", "w") as f:
+            f.write("<html><body><table><tr><th>Nombre de la carpeta</th><th>Tamaño (MB)</th></tr>\n")
+            for folder, size in results:
+                f.write(f"<tr><td>{folder}</td><td>{size / (1024 * 1024):.2f}</td></tr>\n")
+            f.write("</table></body></html>")
+
+        if os.path.exists("tempfile.html"):
+            print("Archivo 'Reporte {folder}.html' creado exitosamente.")
+        else:
+            print("No se pudo crear el archivo 'Reporte {folder}.html'.")
+
+        webbrowser.open_new_tab("Reporte {folder}.html")
+
+        messagebox.showinfo("Listo!", "El proceso ha terminado.")
+    except Exception as e:
+        with open("error-log.txt", "w") as f:
+            f.write(str(e))
+        messagebox.showerror("Error!", f"Ha ocurrido un error.\n\n{str(e)}")
+
+
+def main():
+    root = tk.Tk()
+    root.withdraw()
+
+    progress_window = tk.Toplevel()
+    progress_window.title("Procesando...")
+    progress_label = tk.Label(progress_window, text="Procesando...")
+    progress_label.pack()
+    progress_bar = ttk.Progressbar(progress_window, length=300)
+    progress_bar.pack()
+    progress_bar["value"] = 0
+    progress_bar["maximum"] = 100
+
+    calculate_sizes(progress_label, progress_bar)
+
+    progress_window.destroy()
+
+
+if __name__ == "__main__":
+    main()
